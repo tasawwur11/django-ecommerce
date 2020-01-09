bde2e28847b0430ae14bba297dee4c0a58876a46
tasawwur11 <tasawwur@cybertrontechnologies.com>
2020-01-09 20:22:59 +0530

    v2-13inc

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import home_page
+from .views import home_page,about_page,contact_page
 
 urlpatterns = [
     path('', home_page),
--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -4,6 +4,12 @@ from django.shortcuts import render
 def home_page(request):
 	return render(request, "home_page.html", {})
 
+def about_page(request):
+	return render(request, "home_page.html", {})
+
+def contact_page(request):
+	return render(request, "home_page.html", {})
+
 def home_page_old(request):
 	html_ = """
 	<!doctype html>
